ce5c73bccfe20a3d796088d2e51d2f22b081641d
alexsandro-martinz <martinz.alexsandro@gmail.com>
2024-03-21 03:29:16 -0300

    fix: organiza os componentes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/Index.css";
-import Portifolio from "./Portifolio.jsx";
+import "./Nav";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Content from "./Content";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Portifolio />
+    <div className="container-fluid bg-light">
+      <Nav />
+      <Content />
+      <Footer />
+    </div>
   </React.StrictMode>
 );
