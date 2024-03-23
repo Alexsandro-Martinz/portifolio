b5e3dec56e86f22014fe962c6aa32289f6035136
alexsandro-martinz <martinz.alexsandro@gmail.com>
2024-03-22 21:03:58 -0300

    feat: change bootstrap cdn to npm package

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/css/Index.css";
-import "./Nav";
-import Nav from "./Nav";
-import Footer from "./Footer";
-import Content from "./Content";
+import App from "./App";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="container-fluid bg-light">
-      <Nav />
-      <Content />
-      <Footer />
-    </div>
+    <App />
   </React.StrictMode>
 );
+
+
+
