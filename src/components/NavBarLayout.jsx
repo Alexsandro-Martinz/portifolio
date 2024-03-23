7f82353e76f1e0ddf3618aac3ff91d578fbd731d
alexsandro-martinz <martinz.alexsandro@gmail.com>
2024-03-23 18:45:08 -0300

    feat: close colapsed nav on choice link

--- src/components/NavBarLayout.jsx
+++ src/components/NavBarLayout.jsx
@@ -1,4 +1,4 @@
-import { Nav, Navbar, TabContainer } from "react-bootstrap";
+import { Nav, NavDropdown, Navbar, TabContainer } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import "./NavBarLayout.css";
 import { useEffect } from "react";
@@ -18,27 +18,31 @@ function NavBarLayout() {
         })
     }, [location.pathname]);
 
+    const closeNavOnClick = () => {
+        document.getElementById("basic-navbar-nav").collapse("toggle");
+    }
+
     {
         return (
             <Navbar expand="lg" className="px-3 py-2 fixed-top bg-body-tertiary">
                 <TabContainer>
-                    <Navbar.Brand id="nav-title" onClick={() => document.getElementById("home").click()}>PORTIFÓLIO</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Brand className="" id="nav-title" onClick={() => document.getElementById("home").click()}>PORTIFÓLIO</Navbar.Brand>
+                    <Navbar.Toggle id="toggle" aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav id="nav" className="ms-auto" role="presentation">
+                        <Nav id="nav" className="ms-auto">
 
-                            <div className="d-flex">
-                                <div className="ps-3 nav-item d-inline-flex">
-                                    <Link id="home" className="nav-link" to={""}>Home</Link>
+                            <div className="d-flex pt-3 pt-lg-0 justify-content-between">
+                                <div className="nav-item d-inline-flex">
+                                    <Link onClick={() => closeNavOnClick()} id="home" className="nav-link" to={""}>Home</Link>
                                 </div>
                                 <div className="ps-3 d-inline-flex">
-                                    <Link id="projects" className="nav-link " to="projects">Projetos</Link>
+                                    <Link onClick={() => closeNavOnClick()} id="projects" className="nav-link " to="projects">Projetos</Link>
                                 </div>
                                 <div className="ps-3 d-inline-flex">
-                                    <Link id="contacts" className="nav-link " to="contacts" >Contatos</Link>
+                                    <Link onClick={() => closeNavOnClick()} id="contacts" className="nav-link " to="contacts" >Contatos</Link>
                                 </div>
                                 <div className="ps-3 d-inline-flex">
-                                    <Link id="about" className="nav-link " to="about">Sobre Mim</Link>
+                                    <Link onClick={() => closeNavOnClick()} id="about" className="nav-link " to="about">Sobre Mim</Link>
                                 </div>
                             </div>
 
